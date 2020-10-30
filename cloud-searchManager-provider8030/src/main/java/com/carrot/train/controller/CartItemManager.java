43432572bd5bd10290bd7cb559c2cfa8edf64e2d
carrot <huluobu.cowork@gmail.com>
2020-10-30 10:17:23 +0800

    elastic 关于场次购物车的增删改查

--- cloud-searchManager-provider8030/src/main/java/com/carrot/train/controller/CartItemManager.java
+++ cloud-searchManager-provider8030/src/main/java/com/carrot/train/controller/CartItemManager.java
@@ -1,10 +1,13 @@
 package com.carrot.train.controller;
 
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.carrot.testcloud.entities.CommonResult;
 import com.carrot.train.dao.CartItemRepository;
 import com.carrot.train.entities.CartItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,9 +31,10 @@ public class CartItemManager {
     private CartItemRepository cartItemRepository;
 
     @RequestMapping("/additem")
-    public CommonResult addCartItem() {
+    public CommonResult addCartItem(@RequestBody JSONObject jsonObject) {
         CommonResult result = new CommonResult();
-        CartItem cartItem = new CartItem(1, 1,"动作", 2, "13", new BigDecimal(71));
+        CartItem cartItem = JSONUtil.toBean(jsonObject, CartItem.class);
+//        CartItem cartItem = new CartItem(1, 1,"动作", 2, "13", new BigDecimal(71));
         CartItem recartitem = cartItemRepository.save(cartItem);
         result.setData(recartitem);
         result.setCode(00000);
@@ -39,10 +43,37 @@ public class CartItemManager {
     }
 
     @RequestMapping("/query")
-    public CommonResult queryCartItemById() {
+    public CommonResult queryCartItemById(@RequestBody JSONObject jsonObject) {
+        System.out.println(jsonObject);
         CommonResult result = new CommonResult();
-        CartItem recartitem = cartItemRepository.queryCartItemById("1");
+        String id = jsonObject.getStr("id");
+        CartItem recartitem = cartItemRepository.queryCartItemById(id);
         result.setData(recartitem);
+        result.setCode(00000);
+        result.setMessage("success");
+        return result;
+    }
+
+    @RequestMapping("/update")
+    public CommonResult updateCartItem(@RequestBody JSONObject jsonObject) {
+        CommonResult result = new CommonResult();
+        String id= jsonObject.getStr("id");
+        String filmtype = jsonObject.getStr("filmtype");
+        CartItem recartitem = cartItemRepository.queryCartItemById(id);
+        recartitem.setFilmtype(filmtype);
+        cartItemRepository.save(recartitem);
+        result.setData(recartitem);
+        result.setCode(00000);
+        result.setMessage("success");
+        return result;
+    }
+
+    @RequestMapping("/delete")
+    public CommonResult deleteCartItem(@RequestBody JSONObject jsonObject) {
+        CommonResult result = new CommonResult();
+        String id= jsonObject.getStr("id");
+        cartItemRepository.deleteById(id);
+        result.setData(null);
         result.setCode(00000);
         result.setMessage("success");
         return result;
